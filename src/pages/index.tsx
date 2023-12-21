8da463c52ce947dd2d37f93adfa08f36c53261f1
Pedro_Amado <a86108@ualg.pt>
2023-12-21 14:43:25 +0000

    Alterei o butão da Pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Informações da Oficina ADC ⏱️
           </Link>
         </div>
       </div>
