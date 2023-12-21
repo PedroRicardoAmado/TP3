4828bf6050ae1b74dc94d6d1d57d18d5e0bd0918
Rodrigo <a85280@ualg.pt>
2023-12-21 19:48:25 +0000

    Finalizado docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,6 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'TP3',
+  tagline: 'A Oficina de Confiança!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
