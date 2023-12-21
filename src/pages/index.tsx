81d36428479cd0dfac99fba9f4ec73dd1878a72b
Pedro_Amado <a86108@ualg.pt>
2023-12-21 14:45:33 +0000

    Tentar arrumar o erro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Informações da Oficina ADC ⏱️
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
